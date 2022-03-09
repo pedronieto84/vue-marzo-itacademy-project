61cec3de90169d30ce41d7dcac2d2306f037540c
Jose Nortes <nooors@gmail.com>
2022-03-09 22:18:49 +0100

    it works but with render warn

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="template-card">
+    <b-card
+      header="Projecte"
+      header-tag="projecte"
+      footer="Footer Projecte"
+      :title="title"
+    >
+      <b-card-text>{{ shortDescription }}</b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectCard",
+  props: ["title", "shortDescription"],
+};
+</script>
+
+<style></style>
--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -1,33 +1,64 @@
 <template>
   <div class="home container">
     <!-- The NavComponent goes Here -->
-    <h1 class="text-center">Projects Page</h1>
-    <article class="content">
-      <router-link
-        :to="{ name: 'ProjectDetailPage', params: { projectId: id } }"
-      >
-        <section class="project"></section>
-      </router-link>
-    </article>
+    <h1 class="text-center mt-5 mb-5">Projects Page</h1>
+    <div class="content">
+      <main class="aticle-card" v-for="project in projects" :key="project.id">
+        <router-link
+          :to="{ path: '/ProjectDetailPage/', query: { id: project.id } }"
+        >
+          <project-card
+            :title="project.title"
+            :shortDescription="project.shortExplanation"
+          ></project-card>
+        </router-link>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import store from "@/store";
+import ProjectCard from "@/components/ProjectCard.vue";
 
 export default {
   name: "ProjectsPage",
+  components: { ProjectCard },
   data() {
     return {
       id: 1,
+      projects: [],
     };
+  },
+  created: function () {
+    store.dispatch("getProjects");
+  },
+  computed: {
+    projectsStore: function () {
+      return store.getters.getProjects;
+    },
+  },
+  watch: {
+    projectsStore: function () {
+      // Clon the object in order to avoid mutate state outside the store
+      this.projects = store.getters.getProjects.map((a) => {
+        return { ...a };
+      });
+    },
   },
 };
 </script>
+
 <style lang="sass" scoped>
 .container
   height: 100vh
+
+.content
+  width: 90%
   display: flex
-  justify-content: center
-  align-items: center
+  justify-content: space-between
+
+  a
+    text-decoration: none
 </style>
