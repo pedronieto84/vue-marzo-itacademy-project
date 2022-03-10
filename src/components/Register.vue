f66bd43f980c03df5aeb2afb5c55c73f29dbaac7
Jeniffer-B <69005661+Jeniffer-B@users.noreply.github.com>
2022-03-10 11:24:56 +0100

    maquetación y validación de inputs

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,32 +1,42 @@
 <template>
     <div>
-        <b-form @submit="onSubmit" class="col-8">
-            <FormInput
-                :valor="nombre"
-                :nombre="'Nombre'"
-                @input="nombre = $event"
-                :inputType="'text'" 
-            />
-            <FormInput
-                :valor="email"
-                :nombre="'E-mail'"
-                @input="email = $event"
-                :inputType="'email'" 
-            />
-            <FormInput
-                :valor="password"
-                :nombre="'Password'"
-                @input="password = $event"
-                :inputType="'password'" 
-            />
-            <FormInput
-                :valor="confirmPassword"
-                :nombre="'Confirm Password'"
-                @input="password = $event"
-                :inputType="'password'" 
-            />
-            <button>click</button>
+        <div class=" row justify-content-center align-items-center">
+            <b-form @submit="onSubmit" 
+            class=" m-5 p-5 col-6 border "
+            >
+                <FormInput
+                    :valor="nombre"
+                    :nombre="'Nombre'"
+                    @input="nombre = $event"
+                    :inputType="'text'" 
+                />
+                <FormInput
+                    :valor="email"
+                    :nombre="'E-mail'"
+                    @input="email = $event"
+                    :inputType="'email'" 
+                />
+                <FormInput
+                    :valor="password"
+                    :nombre="'Password'"
+                    @input="password = $event"
+                    :inputType="'password'" 
+                />
+                <FormInput
+                    :valor="confirmPassword"
+                    :nombre="'Confirm Password'"
+                    @input="password = $event"
+                    :inputType="'password'" 
+                />
+                <b-button 
+                    variant="outline-primary"
+                    type="submit" 
+                    value="Submit"
+                >
+                    Registrar
+                </b-button>
         </b-form>
+        </div>  
     </div>
 </template>
 <script>
@@ -43,7 +53,22 @@ export default {
             password: '',
             confirmPassword: '',
         }
-    }
+    },
+    methods: {
+      onSubmit(event){
+        event.preventDefault()
+        this.crearNuevoUsuario();
+      },
+      crearNuevoUsuario(){
+        const nuevoUsuario = {
+          nombre: this.nombre,
+          apellidos: this.apellidos,
+          email: this.email,
+          password: this.password,
+        }
+        console.log(nuevoUsuario)
+      }
+    },
 }
 
 
