8d95cb44284ffe326a1d4c11a0e15faaac5df3df
Jeniffer-B <69005661+Jeniffer-B@users.noreply.github.com>
2022-03-10 12:37:49 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Navbar />
+    <Register />
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -11,9 +12,11 @@
 </template>
 <script>
 import Navbar from '@/components/Navbar.vue'
+import Register from '@/components/Register.vue'
 export default {
   components: {
-    Navbar
+    Navbar,
+    Register
   }
 }
 
