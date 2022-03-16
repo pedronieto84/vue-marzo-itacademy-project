3d6e1c8437ac337dd46b862b4268804327708b1e
Jeniffer-B <69005661+Jeniffer-B@users.noreply.github.com>
2022-03-16 18:47:57 +0100

    linea dispatch newUser

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -133,8 +133,8 @@ export default {
                 password: this.password,
                 organization : this.selected
             }
+            this.$store.dispatch("setNewUser", newUser);
             this.$router.push({path: 'login'})
-            console.log(newUser);
         } else {
             this.showErrorMessage = true
         } 
