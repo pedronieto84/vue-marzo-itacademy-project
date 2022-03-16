a36f06737e6045116fb7668393817e4824a9fb4f
Jeniffer-B <69005661+Jeniffer-B@users.noreply.github.com>
2022-03-16 22:41:27 +0100

    botones login user navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
     <b-navbar class="mt-2">
-      <b-button-group size="lg">
-        <b-button variant="info ">
-          <router-link :to="{ name: 'Admin' }" class="text-white">Admin</router-link>
-        </b-button>
-        <b-button>
-          <router-link :to="{ name: 'ProjectsPage' }" class="text-white">Projects</router-link>
-        </b-button>
-      </b-button-group>
+      <b-button variant="info " v-if="this.$store.state.isAdmin === true">
+        <router-link :to="{ name: 'Admin' }" class="text-white">Admin</router-link>
+      </b-button>
+      <b-button size="lg">
+        <router-link :to="{ name: 'ProjectsPage' }" class="text-white">Projects</router-link>
+      </b-button>
       <!-- Right aligned nav items -->
       <router-link
         :to="{ name: 'CreateProjectPage' }"
         class="btn btn-info ml-auto btn-lg"
         >Create Project</router-link
       >
-      <b-button variant=" ml-auto" size="lg border">
-        <router-link :to="{ name: 'FormLogin' }">Login</router-link>
+      <b-button variant=" ml-auto" size="lg border" v-if="this.$store.state.isLogged === true"> 
+        {{ this.$store.state.userLogged.name}} 
+      </b-button>
+      <b-button variant=" ml-auto" size="lg border" v-else>
+        <router-link :to="{ name: 'FormLogin' }">
+          Login
+        </router-link>
       </b-button>
     </b-navbar>
   </div>
