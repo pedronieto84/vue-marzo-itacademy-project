d806c7db18da995138488c4b125cb06ddc3677b1
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-03-17 15:07:12 +0100

    Proves amb l'array de projectes

--- src/views/MyAccountPage.vue
+++ src/views/MyAccountPage.vue
@@ -31,7 +31,7 @@ export default {
       projects: [
         {
           id: 3,
-          title: "Dolore Omnis Project",
+          title: "Zolore Omnis Project",
           publishedDate: "1650136649",
           deadline: "1656087529",
           shortExplanation:
@@ -42,42 +42,12 @@ export default {
         {
           id: 9,
           title: "Ullam Necessitatibus Voluptatem Project",
-          publishedDate: "1650917598",
-          deadline: "1654657134",
+          publishedDate: 1650917598,
+          deadline: 1654657134,
           shortExplanation:
             "Distinctio quam iusto nemo qui sed. Necessitatibus totam molestiae est ut nostrum.",
           state: "finished",
-          bid: "94919.16",
-        },
-        {
-          id: 17,
-          title: "Quam Nostrum Neque Project",
-          publishedDate: "1649592109",
-          deadline: "1655337139",
-          shortExplanation:
-            "Qui sed animi adipisci beatae quas quia saepe eos. Aut est fuga dignissimos quod.",
-          state: "accepted",
-          bid: "48161.08",
-        },
-        {
-          id: 37,
-          title: "Et Libero Corporis Project",
-          publishedDate: "1650686670",
-          deadline: "1658177622",
-          shortExplanation:
-            "Consequatur est molestiae rem quidem magnam cupiditate quidem. Laudantium neque illo maxime quisquam magnam quia ut.",
-          state: "finished",
-          bid: "1513.81",
-        },
-        {
-          id: 50,
-          title: "Optio Impedit Project",
-          publishedDate: "1650931205",
-          deadline: "1659618602",
-          shortExplanation:
-            "Error voluptatum odio ut a. Tempora voluptatem dolor occaecati.",
-          state: "finished",
-          bid: "9667.78",
+          bid: 94919.16,
         },
       ],
     };
